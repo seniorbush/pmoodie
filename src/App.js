fa2055f6c75d50fddf975091f8f6cd7231457429
Philip Moodie <philipmoodie1989@hotmail.co.uk>
2023-01-04 21:57:54 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import './App.css';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Card from './components/Card'
+import {Fade} from "react-awesome-reveal"
 
 function App() {
   return (
     <div className="App">
 
-      <div className='Home'>
       <Navbar />
+
+      <div className='section'>
       <Hero />
       </div>
 
-      <div className='Projects'>
-      <Card 
-      img={require("./my-project-landing.png")}
-      title="Fuzzy Beginnings Project"
-      description="As an alternative theory, (and because Latin scholars do this sort of thing) someone tracked down a 1914 Latin edition of De Finibus."
-      />
+
+      <div className='section'>
+
+        <Fade duration={1500}>
+        <div className='Projects'>
+        <Card 
+        img={require("./my-project-landing.png")}
+        title="Project #1"
+        description="As an alternative theory, (and because Latin scholars do this sort of thing) someone tracked down a 1914 Latin edition of De Finibus."
+        />
+        <Card 
+        img={require("./my-project-landing.png")}
+        title="Project #2"
+        description="As an alternative theory, (and because Latin scholars do this sort of thing) someone tracked down a 1914 Latin edition of De Finibus."
+        />
+        <Card 
+        img={require("./my-project-landing.png")}
+        title="Project #3"
+        description="As an alternative theory, (and because Latin scholars do this sort of thing) someone tracked down a 1914 Latin edition of De Finibus."
+        />
+        
+        </div>
+        </Fade>
+      
+      </div>
+      
+
+      <div className='section'>
+
+      <Fade duration={1500}>
+        <h2>Contact Page</h2>
+      </Fade>
+      
       </div>
       
     </div>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,24 +1,62 @@
 import styles from "./Hero.module.css"
-import logo from "../assets/pm_logo.png"
+import {Fade, Slide, AttentionSeeker} from "react-awesome-reveal"
+import {FaLinkedin, FaGithub, FaFileDownload} from "react-icons/fa"
+import {GoChevronLeft, GoChevronRight} from "react-icons/go"
+
+// import logo from "../assets/pm_logo.png"
 
 
 
 export default function Hero(){
     return (
-        <div className={styles.hero}>
+        <div className={styles.main}>
+            <Slide cascade direction="down">
+            <AttentionSeeker effect="pulse" 
+                duration={1150} 
+                delay={1100} 
+                >
+                    <h1>PHILIP MOODIE</h1>
+                </AttentionSeeker>
+                
+                <AttentionSeeker effect="pulse" 
+                delay={1100} 
+                duration={1150}>
+                    <h2>
+                        <span>W</span>
+                        <span>E</span>
+                        <span>B</span>
+                        <span> </span>
+                        <span>D</span>
+                        <span>E</span>
+                        <span>V</span>
+                        <span>E</span>
+                        <span>L</span>
+                        <span>O</span>
+                        <span>P</span>
+                        <span>E</span>
+                        <span>R</span>
+                    </h2>
+                </AttentionSeeker>
 
-            <div className={styles.leftSide}>
-                <h1>PHILIP MOODIE</h1>
-                <h2>WEB DEVELOPER</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vestibulum, arcu in scelerisque luctus, lacus turpis scelerisque nisi, eu lacinia nisl nibh at odio. Ut laoreet accumsan nisi sed dignissim. Phasellus turpis justo, fermentum ut quam eget, feugiat porttitor sapien. Sed risus nunc, porta ullamcorper consequat quis, sodales sit amet nibh.</p>
-                <button>Download CV</button>
-            </div>
-            
+            </Slide>
 
-            <div className={styles.rightSide}>
-                <img src={logo} alt=""/>
-            </div> 
-            
+          <Fade delay={1900}>
+                
+                <p>Hi there!  My name is Philip and I am a web developer from Northern Ireland
+                    with a passion for creating intuitive and  
+                    visually stunning websites.  I have always been fascinated by design 
+                    and technology, web development has helped me to merge those 
+                    two interests.  I love to learn and stay up-to-date with the 
+                    latest industry trends.</p>
+
+                <div className={styles.icons}>
+                    <a href="https://www.linkedin.com/in/pmoodie/" target="_blank" rel="noreferrer"><FaLinkedin className={styles.linkedin}/></a>
+                    <a href="https://github.com/seniorbush" target="_blank" rel="noreferrer"><FaGithub className={styles.github}/></a>
+                    <FaFileDownload className={styles.downloadCV}/>
+                </div>
+                          
+          </Fade>
+             
         </div>
     )
 }
