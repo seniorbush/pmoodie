e655a9bcacbf66b05eb4f89cc00d625e145e4ac9
Philip Moodie <philipmoodie1989@hotmail.co.uk>
2023-01-06 12:55:34 +0000

    new card styling added

--- src/App.js
+++ src/App.js
@@ -3,36 +3,73 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Card from './components/Card'
 import {Fade} from "react-awesome-reveal"
+import { DiHtml5, DiCss3, DiJsBadge, DiNodejsSmall, DiMongodb } from "react-icons/di";
 
 function App() {
+  const techList1 = [
+    <DiHtml5 className="tech"/>,
+    <DiCss3 className="tech"/>,
+    <DiJsBadge className="tech"/>,
+    <DiNodejsSmall className="tech"/>,
+    <DiMongodb className="tech"/>
+  ]
+
+  const techList2 = [
+    <DiHtml5 className="tech"/>,
+    <DiCss3 className="tech"/>,
+    <DiJsBadge className="tech"/>,
+  ]
+
   return (
     <div className="App">
 
       <Navbar />
 
       <div className='section'>
-      <Hero />
+        <Hero />
+        
       </div>
-
+           
 
       <div className='section'>
 
         <Fade duration={1500}>
         <div className='Projects'>
+        
         <Card 
-        img={require("./my-project-landing.png")}
-        title="Project #1"
+        img={require("./assets/basket(2).png")}
+        title="EasyBasket"
+        tech={techList2}
         description="As an alternative theory, (and because Latin scholars do this sort of thing) someone tracked down a 1914 Latin edition of De Finibus."
         />
+
         <Card 
-        img={require("./my-project-landing.png")}
-        title="Project #2"
-        description="As an alternative theory, (and because Latin scholars do this sort of thing) someone tracked down a 1914 Latin edition of De Finibus."
+        img={require("./assets/crypto_app(2).png")}
+        github="https://github.com/seniorbush/blockberry"
+        live="https://blockberry.onrender.com/"
+        title="Blockberry"
+        tech={techList1}
+        description="This cryptocurrency pricing and portfolio website uses 
+        the CoinGecko and Newsdata.io APIs to provide real-time information 
+        about various cryptocurrencies. It also incorporates MongoDB for user 
+        authentication and validation. The website is built using Node.js, 
+        Express.js, and EJS, with Chart.js and Bootstrap for styling and layout. 
+        Users can create an account and log in to create a personalized portfolio 
+        of their cryptocurrency holdings, which can be tracked and updated over time."
         />
+
         <Card 
-        img={require("./my-project-landing.png")}
-        title="Project #3"
-        description="As an alternative theory, (and because Latin scholars do this sort of thing) someone tracked down a 1914 Latin edition of De Finibus."
+        img={require("./assets/loan_app(2).png")}
+        github="https://github.com/seniorbush/Loan_Calculator"
+        live="https://leave-me-a-loan.netlify.app/"
+        title="Loan Calculator"
+        tech={techList2}
+        description="This responsive web application allows users to 
+        calculate their monthly loan repayments using HTML, CSS, and 
+        JavaScript. The user is able to input various loan details, 
+        including the principal amount, interest rate, and loan term, 
+        and the application will calculate the monthly repayment amount 
+        based on these inputs"
         />
         
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,30 @@
+import {FaGithub} from "react-icons/fa"
+import {TbExternalLink} from "react-icons/tb"
 import styles from "./Card.module.css"
 
 export default function Card(props){
     return (
         <div className={styles.card}>
             <div className={styles.cardBody}>
+                <div className={styles.callToAction}>
                 <img className={styles.cardImg} src={props.img} alt=""/>
-                <h2 className={styles.cardTitle}>{props.title}</h2>
-                <p className={styles.cardDescription}>{props.description}</p>
+                <div className={styles.buttons}>
+                    <button className={styles.cardLinkLeft}><a href={props.github} target="_blank" rel="noreferrer"><FaGithub/> Github</a></button>
+                    <button className={styles.cardLinkRight}><a href={props.live} target="_blank" rel="noreferrer"><TbExternalLink/> Live Site</a></button>
+                </div>
             </div>
-            <button className={styles.cardLinkLeft}>Github</button>
-            <button className={styles.cardLinkRight}>Live Site</button>
+                <h2 className={styles.cardTitle}>{props.title}</h2>
+                <div className={styles.techList}>{props.tech}</div>
+                <h4 className={styles.titles}>Description:</h4>
+                <p className={styles.cardDescription}>{props.description}</p>
+                {/* <h4 className={styles.titles}>Technologies used:</h4> */}
+                
+            </div>
+            
+            
 
         </div>
         
     )
 }
+
